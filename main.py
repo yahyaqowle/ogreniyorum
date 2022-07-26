1e6f4f5c4193f985f45d7a4985e3868212d23d8b
Qowle <codert50@gmail.com>
2022-07-27 00:04:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("hello World")
+print("im Yaya Qowle")
+
+first_name = "Yaya"
+last_name = "Qowle"
+full_name = first_name +" "+ last_name
+print("Hello "+full_name)
+#print(type(full_name))
+
+age = 23
+age += 1
+print("Your age is: "+str(age))
+print(type(age))
+
+height = 1.75
+print('Your height is: '+str(height)+'meter')
+print(type(height))
+
+human = True
+animal = False
+#earth = human + animal
+print("Are you a human: "+str(human))
+

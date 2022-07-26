df5eee4b8c922b4541211396081c4b36a814654e
Qowle <codert50@gmail.com>
2022-07-27 00:27:24 +0300

    practice 1.1

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ age += 1
 print("Your age is: "+str(age))
 print(type(age))
 
-height = 1.75
+height = 15.75
 print('Your height is: '+str(height)+'meter')
 print(type(height))
 
+weight = 500
+print('Your weight is: '+str(weight)+"kg")
+
 human = True
-animal = False
-#earth = human + animal
+animal = True
+internet = False
+earth = human + animal
 print("Are you a human: "+str(human))
+print("Is human and animal live in earth: "+str(earth))
+
 

f53481cb48f0a9b189d39d20ab8966916e5f76e7
Qowle <codert50@gmail.com>
2022-07-27 16:49:24 +0300

    Merge branch 'main' of https://github.com/yahyaqowle/ogreniyorum

--- main.py
+++ main.py
@@ -1,29 +1,76 @@
+# Day1 #26/7/22
+# Eng/ variable # Som/ doorsoome/wax-matale
+
 print("hello World")
 print("im Qowle")
+print(" I will use for my comments both English and Somali")
 
 first_name = "dost"
 last_name = "ayse"
 full_name = first_name +" "+ last_name
 print("Hello "+full_name)
-print(type(full_name))
+#to check the type
+# print(type(full_name))
 
-age = 23
+age = 29
 age += 1
 print("Your age is: "+str(age))
 print(type(age))
 
-height = 15.75
+height = 56.98
 print('Your height is: '+str(height)+'meter')
-print(type(height))
+# print(type(height))
 
 weight = 500
-print('Your weight is: '+str(weight)+"kg")
+print('Your weight is: '+str(weight)+"kg/lbs")
 
 human = True
 animal = True
 internet = False
 earth = human + animal
 print("Are you a human: "+str(human))
-print("Is human and animal live in earth: "+str(earth))
+print("How many of living above live in earth: "+str(earth))
+
+#3.str #Day2 27/7/22
+#multiple assignments = allows us to assign multiple variables at the same time in line of code
+#dhowr shaqood = wuxuu kuu ogolaan inaad koox doorsamayaal/matalayaal(variables) ah aad badasho.
+
+name2 = "Beerlula2"
+age2 = 43
+attractive2 = True
+print(name2)
+print(age2)
+print(attractive2)
+
+#example 2 #tusaale 2aad
+name3, age3, attractive3 = "Beerlula3", 46, True
+print(name3, age3, attractive3)
+
+#instead of writing 8lines of code u can reduce to 2 lines of code and is done by multiple assignment
+#halkii aad 8 sadar oo kood ah qori laheed waxaa kusoo gaabin karta 2 sadar adoo isticmaalya dhowr-shaqood
+Spongebob = 685
+Patrick = 685
+Sandy = 685
+Squidgame = 685
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidgame)
+#example 2 of Spongebob's
+Spongebob1 = Patrick1 = Sandy1 = Squidgame1 = 35
+print(Spongebob1, Patrick1, Sandy1, Squidgame1)
+
+#4.string method #Day2 27/7/22
+
+
+
+
+
+
+
+
+
+
 
 

79206bb71be0986c9bb31a92f69564f8d19b55a4
Qowle <codert50@gmail.com>
2022-09-04 16:35:06 +0300

    web scraping

--- csvextract.py
+++
@@ -1,6 +0,0 @@
-report = open('A words.csv', 'w', encoding='utf-8')
-f = open("A words.txt", "r", encoding='utf-8')
-
-convert = f.read().split()
-report.write(str(convert))
-report.close()
--- vocab.py
+++
@@ -1,6 +0,0 @@
-report = open('C words.txt', 'w', encoding='utf-8')
-
-with open('change to word.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        report.write(line.split(' ')[0])
-report.close()

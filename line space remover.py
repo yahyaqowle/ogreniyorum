18d6db98436fc5346e87765233a9d6d431603ad2
Qowle <codert50@gmail.com>
2022-10-11 02:56:44 +0300

    web scraping

---
+++ line space remover.py
@@ -0,0 +1,13 @@
+# filepath = "A words Qaamuus.txt"
+#
+# with open(filepath, 'w', encoding='utf-8') as f:
+#     for line in filepath:
+#         if not line.isspace():
+#             filepath.write(line)
+
+with open("A words Qaamuus.txt","r", encoding='utf-8') as f, open("outfile.txt","w", encoding='utf-8') as outfile:
+    for i in f.readlines():
+        if not i.strip():
+            continue
+        if i:
+            outfile.write(i)

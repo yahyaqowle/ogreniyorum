fcb1c2f13f883bb5c768736a6678b79f3b01beb2
Qowle <codert50@gmail.com>
2022-12-27 16:30:20 +0300

    test

--- 2.Dictionary to words.py
+++ 2.Dictionary to words.py
@@ -1,6 +1,21 @@
-report = open('A words VOCAB.txt', 'w', encoding='utf-8')
+# report = open('A words VOCAB.txt', 'w', encoding='utf-8')
+#
+# with open('A words DB.txt', 'r', encoding='utf-8') as f:
+#     for line in f:
+#         report.write(line.split()[0])
+# report.close()
 
-with open('A words Qaamuus.txt', 'r', encoding='utf-8') as f:
+# Open the output file in write mode
+with open('A words VOCAB.txt', 'w', encoding='utf-8') as report:
+  # Open the input file in read mode
+  with open('A words DB.txt', 'r', encoding='utf-8') as f:
+    # Iterate over the lines in the input file
     for line in f:
-        report.write(line.split(' ')[0])
-report.close()
+      # Split the line into a list of words
+      words = line.split()
+      # Extract the first word from the list
+      first_word = words[0]
+      # Write the first word to the output file on a separate line
+      report.write(first_word + '\n')
+
+

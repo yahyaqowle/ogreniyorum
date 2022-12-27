f37926a8f6e9b70fb81f518318d02fcc8376e51b
Qowle <codert50@gmail.com>
2022-12-27 14:12:25 +0300

    web scraping

--- line space remover.py
+++ line space remover.py
@@ -7,7 +7,7 @@
 
 
 
-with open("B words Qaamuus.txt","r", encoding='utf-8') as f, open("outfile.txt","w", encoding='utf-8') as outfile:
+with open("A words Qaamuus.txt","r", encoding='utf-8') as f, open("A words DB.txt","w", encoding='utf-8') as outfile:
     for i in f.readlines():
         if not i.strip():
             continue

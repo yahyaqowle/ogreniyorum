141c4784707f4a7ae3076a052c54b75443ba6ce5
Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-01-24 09:37:52 +0300

    Updated Dict-word Script

--- 2.Dictionary to words.py
+++ 2.Dictionary to words.py
@@ -1,21 +1,34 @@
-# report = open('A words VOCAB.txt', 'w', encoding='utf-8')
+# report = open('B words VOCAB.txt', 'w', encoding='utf-8')
 #
-# with open('A words DB.txt', 'r', encoding='utf-8') as f:
+# with open('B words Qaamuus.txt', 'r', encoding='utf-8') as f:
 #     for line in f:
 #         report.write(line.split()[0])
 # report.close()
 
-# Open the output file in write mode
-with open('A words VOCAB.txt', 'w', encoding='utf-8') as report:
+# # Open the output file in write mode
+# with open('B words VOCAB.txt', 'w', encoding='utf-8') as report:
+#   # Open the input file in read mode
+#   with open('B words Qaamuus.txt', 'r', encoding='utf-8') as f:
+#     # Iterate over the lines in the input file
+#     for line in f:
+#       # Split the line into a list of words
+#       words = line.split()
+#       # Extract the first word from the list
+#       first_word = words[0]
+#       # Write the first word to the output file on a separate line
+#       report.write(first_word + '\n')
+
+with open('B words VOCAB.txt', 'w', encoding='utf-8') as report:
   # Open the input file in read mode
-  with open('A words DB.txt', 'r', encoding='utf-8') as f:
+  with open('B words Qaamuus.txt', 'r', encoding='utf-8') as f:
     # Iterate over the lines in the input file
     for line in f:
       # Split the line into a list of words
       words = line.split()
-      # Extract the first word from the list
-      first_word = words[0]
-      # Write the first word to the output file on a separate line
-      report.write(first_word + '\n')
-
+      # Check if the list is not empty
+      if words:
+        # Extract the first word from the list
+        first_word = words[0]
+        # Write the first word to the output file on a separate line
+        report.write(first_word + '\n')
 

2b7b51a52ed31eb3936d97b8fe293a54852bd21c
Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-04-22 03:22:43 +0300

    revolutionary and milestone

--- 3.Words to csv.py
+++ 3.Words to csv.py
@@ -1,5 +1,5 @@
 report = open('E words VOCAB.csv', 'w', encoding='utf-8')
-f = open("E words VOCAB.txt", "r", encoding='utf-8')
+f = open("pull.txt", "r", encoding='utf-8')
 
 convert = f.read().split()
 report.write(str(convert))

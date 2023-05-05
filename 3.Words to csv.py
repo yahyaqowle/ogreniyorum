5e3b5383ac15bda69d1622db3b83f6b560597fe3
Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-05-05 04:20:52 +0300

    revolutionary and milestone

--- 3.Words to csv.py
+++ 3.Words to csv.py
@@ -1,5 +1,5 @@
-report = open('E words VOCAB.csv', 'w', encoding='utf-8')
-f = open("pull.txt", "r", encoding='utf-8')
+report = open('Cleaned Somali words/Somali-Words.csv', 'w', encoding='utf-8')
+f = open("Somali Words/Somali-Words.txt", "r", encoding='utf-8')
 
 convert = f.read().split()
 report.write(str(convert))

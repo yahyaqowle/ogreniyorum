72163a193208992205e699fb180ab4a4a5b623b2
Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-05-08 11:50:16 +0300

    all

--- 3.Words to csv.py
+++ 3.Words to csv.py
@@ -1,5 +1,5 @@
-report = open('Cleaned Somali words/Somali-Words.csv', 'w', encoding='utf-8')
-f = open("Somali Words/Somali-Words.txt", "r", encoding='utf-8')
+report = open('Cleaned Somali words/Somali-Dictionary.csv', 'w', encoding='utf-8')
+f = open("Somali Words/Somali-Dictionary.txt", "r", encoding='utf-8')
 
 convert = f.read().split()
 report.write(str(convert))

fcfe4f6ed6ab1052a398eeb6ac0ebb049bdb8428
Yahya Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-05-21 15:59:16 +0300

    init334

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -39,37 +39,46 @@ options = ('rock', 'paper', 'scissors')
 game_on = True
 
 while game_on:
-    player = True
-    computer = True
+    Froz = True
+    Qowle = True
 
-    while player and computer not in options:
-        player = input("Mid dooro PLAYer (rock, paper, scissors): ")
-        computer = input("Mid dooro Comp (rock, paper, scissors): ")
+    while Froz and Qowle not in options:
+        Froz = input("Froz: Mid dooro (rock, paper, scissors): ")
+        Qowle = input("Qowle:  Mid dooro  (rock, paper, scissors): ")
 
-    print(f'Player: {player}')
-    print(f'Computer: {computer}')
+    print(f'Froz: {Froz}')
+    print(f'Qowle: {Qowle}')
 
-    # if player == computer:
+    # if Froz == Qowle:
     #     print("it's a tie")
-    # elif player == "rock" and computer == "scissors":
+    # elif Froz == "rock" and Qowle == "scissors":
     #     print("waad guulaysatay")
-    # elif player == "paper" and computer == "rock":
+    # elif Froz == "paper" and Qowle == "rock":
     #     print("waad guulaysatay")
-    # elif player == "scissors" and computer == "paper":
+    # elif Froz == "scissors" and Qowle == "paper":
     #     print("waad guulaysatay")
     # else:
     #     print("waad khashirtay")
 
-    if player == computer:
+    if Froz == Qowle:
         print("It's a tie!")
-    elif (player == "rock" and computer == "scissors") or (player == "paper" and computer == "rock") or (player == "scissors" and computer == "paper"):
-        print("Player wins!")
+    elif (Froz == "rock" and Qowle == "scissors") or (Froz == "paper" and Qowle == "rock") or (Froz == "scissors" and Qowle == "paper"):
+        print("Froz wins!")
     else:
-        print("Computer wins!")
+        print("Qowle wins!")
 
-    if not input("Markale ciyaar? (y/n): ").lower() == "y":
+    # if not input("Markale ciyaar? (y/n): ").lower() == "y":
+    #     game_on = False
+    #     # break
+
+    play_again = input("Markale ciyaar? (y/n): ").lower()
+    if play_again == "y":
+        game_on = True
+    elif play_again == "n":
         game_on = False
-        # break
+    elif not play_again == "y" or "n":
+        print(play_again)
+        break
 
 print("Mahadsanid!")
 
